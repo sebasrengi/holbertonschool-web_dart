af3571d94cb0734668ac661a398f042b67bfe320
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2023-03-21 23:23:34 -0500

    Update 1-get_user_id.dart

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,15 +1,6 @@
 import 'dart:convert';
+import '1-util.dart';
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-getUserId() async {
-  var strRepr = await fetchUserData();
-
-  var Repr  = json.decode(strRepr);
-
-  return Repr["id"];
+Future<String> getUserId() {
+  return fetchUserData().then((user) => json.decode(user)['id']);
 }
