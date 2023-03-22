96fcd591372c85716f208fcc00f2cb8b142bcc72
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2023-03-21 23:25:00 -0500

    Update 4-get_sum.dart

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,48 +1,27 @@
-import 'dart:convert';
-import 'dart:async';
-import 'dart:core';
+import "4-util.dart";
+import "dart:convert";
 
-Future<String> fetchUserOrders(String id) async {
-  var orders = {
-    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
-  };
+Future<double> calculateTotal() async {
   try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(orders[id]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<String> fetchProductPrice(product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(products[product]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-calculateTotal() async {
-    try {
-        double price = 0;
-	
-        final Map<String, dynamic> userData = json.decode(await fetchUserData());
-        final String data = userData['id'];
-        final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
-        for (int idx = 0; idx < userOrder.length; idx++) {
-            price += json.decode(await fetchProductPrice(userOrder[idx]));
+    // Call fetchUserData
+    return fetchUserData().then((user) {
+      // Convert string response to json
+      String id = json.decode(user)["id"];
+      // Asynchronously call fetchUserOrders with id of user from fetchUserData
+      return fetchUserOrders(id).then((products) async {
+        double totalPrice = 0.0;
+        // Convert string response to json and loop through product list
+        for (String product in json.decode(products)) {
+          // As each product is fetched, call fetchProductPrice with product
+          await fetchProductPrice(product).then((price) {
+            // Convert string response to json and add price to total
+            totalPrice += json.decode(price);
+          });
         }
-        return price;
-    } catch (err) {
-        print('error caught: $err');
-        return -1;
-    }
+        return totalPrice;
+      });
+    });
+  } catch (e) {
+    return -1;
+  }
 }
